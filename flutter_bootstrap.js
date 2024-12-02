7248f025d7e7bc70b42d482ab083f50d219a18d1
SHAZIN _ <chachushazin516@gmail.com>
2024-12-02 17:12:41 +0530

    1.4.4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "19716908"
+    serviceWorkerVersion: "2846705821"
   }
 });
